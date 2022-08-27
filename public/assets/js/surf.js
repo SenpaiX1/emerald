95d27c366ca83a0d2ef38f150a2d6fc742a946b3
frog22222 <106568662+frog22222@users.noreply.github.com>
2022-08-27 09:13:29 -0400

    Update surf.js

--- public/assets/js/surf.js
+++ public/assets/js/surf.js
@@ -5,26 +5,11 @@
 /* ----------------------------------------------- */
 $ = e => document.getElementById(e) || [];
 
-$("tacogithub").href = "https://github.tacoproxy.ml";
-
-//Alloy Default
-
-$('alloyframe').onclick = function() {
-    var frame = document.getElementById("frame");
-    var url = $('iurl').value;
-    var det = document.domain;
-    var domain = det.replace('www.', '').split(/[/?#]/)[0];
-    const origin = btoa(url)
-    frame.src = "https://" + domain + "/prefix/" + origin;
-    frame.style['visibility'] = "visible";
-    return false;
-};
-
-//Alloy Frame
+//Alloy default
 
 $('alloydefault').onclick = function() {
     var frame = document.getElementById("frame");
-    var url = $('iurl').value;
+    var url = "https://" + $('iurl').value;
     var det = document.domain;
     var domain = det.replace('www.', '').split(/[/?#]/)[0];
     const origin = btoa(url)
